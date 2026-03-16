89a2f2b3f4b047f99af42e4ccbb1554f6e93ddad
Sai Vikas Reddy Dundumulla <saivikasreddy987@gmail.com>
2026-03-16 15:33:33 +0530

    add order state changing ops & Order Service

--- common/common-domain/src/main/java/com/food/ordering/system/domain/event/DomainEvent.java
+++ common/common-domain/src/main/java/com/food/ordering/system/domain/event/DomainEvent.java
@@ -1,4 +1,4 @@
 package com.food.ordering.system.domain.event;
 
-public interface DomainEvent<T> {
+public interface DomainEvent<T> { // this is a marker interface
 }
---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,48 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class OrderDomainServiceImpl implements OrderDomainService {
+    @Override
+    public OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order, restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of("UTC")));
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        return null;
+    }
+
+    @Override
+    public void cancelOrder(Order order) {
+
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+    }
+
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+    }
+}
